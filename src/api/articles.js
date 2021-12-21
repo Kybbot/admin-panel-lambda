61e4254b11579c9546d0e330ded7af4bfb7a67a6
Артём Одийко <odiykoartem@gmail.com>
2021-12-21 17:33:17 +0200

    removed excess requests

--- src/api/articles.js
+++ src/api/articles.js
@@ -1,35 +1,15 @@
-const API_URL = "https://x03xdh6x33.execute-api.us-east-1.amazonaws.com/dev";
+import api from "../constants/api";
 
 function getAllArticles() {
-	return fetch(`${API_URL}/articles`, {
+	return fetch(`${api.url}/articles`, {
 		method: "GET",
 	});
 }
 
 function getArticle(id = "") {
-	return fetch(`${API_URL}/articles/${id}`, {
+	return fetch(`${api.url}/articles/${id}`, {
 		method: "GET",
 	});
 }
 
-function updateArticle(id = "", data = {}) {
-	return fetch(`${API_URL}/articles/${id}`, {
-		method: "PUT",
-		headers: {
-			"Content-type": "application/json",
-		},
-		body: JSON.stringify(data),
-	});
-}
-
-function addArticle(data = {}) {
-	return fetch(`${API_URL}/articles/create`, {
-		method: "POST",
-		headers: {
-			"Content-type": "application/json",
-		},
-		body: JSON.stringify(data),
-	});
-}
-
-export { getAllArticles, getArticle, addArticle, updateArticle };
+export { getAllArticles, getArticle };
