200b2218c61cc28390a57813852c1d19e6709a3e
Артём Одийко <odiykoartem@gmail.com>
2021-12-22 18:53:01 +0200

    added link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 
 const Header = () => {
 	const navigate = useNavigate();
@@ -15,7 +15,11 @@ const Header = () => {
 		<header className="header">
 			<div className="container">
 				<div className="header__container">
-					<h1 className="header__title">Lambda admin-panel</h1>
+					<h1 className="header__title">
+						<Link className="header__link" to="/">
+							Lambda admin-panel
+						</Link>
+					</h1>
 					{!checkLocation && (
 						<button className="btn" type="button" onClick={buttonHandler}>
 							Создать статью
