6f051ca7d3bc53ae511c5c9bb57e2d9a208010ce
Kybbot <odiykoartem@gmail.com>
2021-12-24 14:40:46 +0200

    update hook

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,39 +1,45 @@
 import React from "react";
-
-import api from "../constants/api";
+import { typesOfData } from "../constants";
 
 const useFetch = () => {
 	const [loading, setLoading] = React.useState(false);
 	const [error, setError] = React.useState(null);
 
 	const request = React.useCallback(
-		async (url, method = "GET", body = null, headers = {}) => {
+		async (url, method = "GET", body = null, headers = {}, type) => {
 			setLoading(true);
 
 			try {
-				if (body) {
-					body = JSON.stringify(body);
-					headers["Content-Type"] = "application/json";
-					headers["Authorization"] = `Basic ${btoa("admin:qwerty123")}`;
+				if (type !== typesOfData.img) {
+					if (body) {
+						body = JSON.stringify(body);
+						headers["Content-Type"] = "application/json";
+						headers["Authorization"] = `Basic ${btoa("admin:qwerty123")}`;
+					}
 				}
 
-				const response = await fetch(`${api.url}${url}`, {
+				const response = await fetch(`${url}`, {
 					method,
 					body,
 					headers,
 				});
-				const data = await response.json();
 
-				if (!response.ok) {
-					throw new Error(data.message || "Что-то пошло не так");
+				if (response.status !== 204) {
+					const data = await response.json();
+
+					if (!response.ok) {
+						throw new Error(data.message || "Что-то пошло не так");
+					}
+
+					setLoading(false);
+
+					return data;
+				} else {
+					setLoading(false);
 				}
-
-				setLoading(false);
-
-				return data;
 			} catch (err) {
-				setLoading(false);
 				setError(err.message);
+				setLoading(false);
 			}
 		},
 		[]
